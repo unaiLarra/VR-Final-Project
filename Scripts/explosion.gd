4ca203a137fba854772db7833fea0f3f0e243227
unaiLarra <113512581+unaiLarra@users.noreply.github.com>
2024-12-11 16:04:46 +0200

    Added eye dependant sparks on the explosion

--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -1,4 +1,12 @@
 extends Node3D
 
+@export var time_scale: float
+@export var duration: float
+
 func explode() -> void:
 	%AnimationPlayer.play("Explode")
+
+func frame_freeze() -> void:
+	Engine.time_scale = time_scale
+	await  get_tree().create_timer(duration*time_scale).timeout
+	Engine.time_scale = 1.0
